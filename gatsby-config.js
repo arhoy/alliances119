2a68b856e5070d88c0281940102e392d53f4af9f
arhoy <arhoy@ualberta.ca>
2019-11-05 13:02:28 -0700

    added chat

--- gatsby-config.js
+++ gatsby-config.js
@@ -110,7 +110,7 @@ module.exports = {
         websiteId: process.env.CRISP_WEBSITE_ID,
         // enableDuringDevelop: false, // Optional. Disables Crisp Chat during gatsby develop. Defaults to true.
         // defer: true, // Optional. Sets the Crisp loading script to defer instead of async. Defaults to false.
-        // enableImprovedAccessibility: false // Optional. Sets aria-label attribute on pop-up icon for screen readers. Defaults to true.
+        // enableImprovedAccessibility: false // Optional. Sets aria-label attribute on pop-up icon for screen readers. Defaults to true
       },
     },
   ],
