96906c1b4301e5dc2f558f4dea5135a420d5fab0
arhoy <arhoy@ualberta.ca>
2019-11-05 13:00:17 -0700

    added chat in dev and prod

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,5 +104,14 @@ module.exports = {
         endpoint: process.env.MAILCHIMP_ENDPOINT,
       },
     },
+    {
+      resolve: 'gatsby-plugin-crisp-chat',
+      options: {
+        websiteId: process.env.CRISP_WEBSITE_ID,
+        // enableDuringDevelop: false, // Optional. Disables Crisp Chat during gatsby develop. Defaults to true.
+        // defer: true, // Optional. Sets the Crisp loading script to defer instead of async. Defaults to false.
+        // enableImprovedAccessibility: false // Optional. Sets aria-label attribute on pop-up icon for screen readers. Defaults to true.
+      },
+    },
   ],
 };
